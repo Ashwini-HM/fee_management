42bd5a72f77d3404186430520be02f441e05415a
Ashwini <gowdaash30@gmail.com>
2026-08-18 21:59:02 +0530

    Create SmartFeeManagementApplication.java

---
+++ src/main/java/com/example/smart_fee_management/SmartFeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.smart_fee_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFeeManagementApplication.class, args);
+	}
+
+}
